482371d14f35a15f9c62daac77f79fd437af7c3d
gabyferr <70179462+gabyferr@users.noreply.github.com>
2023-05-03 22:12:51 -0300

    Adicionar arquivos de projeto.

---
+++ lanchoneteCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lanchoneteCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ lanchoneteCSharp/Views/Shared/carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lanchoneteCSharp.Views.Shared
+{
+    public class carouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
